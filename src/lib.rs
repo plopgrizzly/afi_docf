0736c0194d58ecd9635435bf327d8d33edc71cbc
Jeron Aldaron Lau <jeron.lau@plopgrizzly.com>
2017-10-08 11:06:35 -0500

    Added files.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// Aldaron's Format Interface / Aldaron's Document Format (docf)
+// Copyright (c) 2017 Plop Grizzly, Jeron Lau <jeron.lau@plopgrizzly.com>
+// Licensed under the MIT LICENSE
+//
+// src/lib.rs
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
